c5e1ea4b7d143aa66459621d76a98e80b56bc924
zahra ellioun <zizi.ell75@gmail.com>
2025-08-12 16:10:10 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoadMoreButton from "./components/load-more-button";
+
+function App() {
+  return (
+    <div className="App">
+      <LoadMoreButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/load-more-button/index.jsx
@@ -0,0 +1,5 @@
+const LoadMoreButton = () => {
+  return <div>load more button</div>;
+};
+
+export default LoadMoreButton;
